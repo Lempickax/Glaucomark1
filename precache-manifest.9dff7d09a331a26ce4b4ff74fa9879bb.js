af373c79f41c8d66938906caa7de0d8dd8a802ae
Tony Dang <tonydang0124@gmail.com>
2020-07-25 23:52:06 -1000

    Updates

---
+++ precache-manifest.9dff7d09a331a26ce4b4ff74fa9879bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d1fea660451e8f3beb17a30ad1f530",
+    "url": "/glaucomark/index.html"
+  },
+  {
+    "revision": "b178e5e14857826e8482",
+    "url": "/glaucomark/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7f3fbb7db70f2bab745",
+    "url": "/glaucomark/static/css/main.55dc6073.chunk.css"
+  },
+  {
+    "revision": "b178e5e14857826e8482",
+    "url": "/glaucomark/static/js/2.29f9df79.chunk.js"
+  },
+  {
+    "revision": "03d8dcb4c6506bee8b0de1d30ba21dfc",
+    "url": "/glaucomark/static/js/2.29f9df79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f3fbb7db70f2bab745",
+    "url": "/glaucomark/static/js/main.21d840e2.chunk.js"
+  },
+  {
+    "revision": "b20bba423b0a8c1644f8",
+    "url": "/glaucomark/static/js/runtime-main.86e28947.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/glaucomark/static/media/logo.5d5d9eef.svg"
+  }
+]);
